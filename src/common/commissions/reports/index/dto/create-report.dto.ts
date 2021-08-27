b556a46d44171882fd48607afb4deeb6371d3279
= <iresonancei@mail.ru>
2021-08-27 11:51:49 +0300

    Save

--- src/common/commissions/reports/index/dto/create-report.dto.ts
+++ src/common/commissions/reports/index/dto/create-report.dto.ts
@@ -1,4 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+import * as faker from "faker";
+import { IsNotEmpty, IsNumber} from "class-validator";
+
 export class CreateReportDto {
+
+    @ApiProperty({example: faker.datatype.number(20)})
+    @IsNumber()
+    @IsNotEmpty()
     userId: number
+
+    @ApiProperty({example: faker.datatype.number(20), required: false})
+    @IsNumber()
+    @IsNotEmpty()
     commissionId: number
 }
